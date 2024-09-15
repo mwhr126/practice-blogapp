74681df84652258e94dcd042acdfc36f498c9f57
miwa <dskipineapple@gmail.com>
2024-09-15 21:38:21 +0900

    記事を更新できるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
 
 end
